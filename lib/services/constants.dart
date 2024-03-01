59bc10a68f84d5d2d20111c8f7f11f3e4a578dbd
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-02-29 21:50:23 -0300

    first commit
    Screen login

---
+++ lib/services/constants.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const String logoImage = 'assets/images/logo.png';
+const String faceImage = 'assets/images/facebook.png';
+const String googleImage = 'assets/images/google.png';
+const Color mainColor = Color.fromARGB(255, 255, 135, 171);
+const InputDecoration textFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.all(16),
+  focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15)),
+    borderSide: BorderSide(
+      color: Color.fromARGB(255, 255, 135, 171),
+      width: 1,
+    ),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15)),
+    borderSide: BorderSide(color: Color.fromARGB(255, 255, 135, 171), width: 1),
+  ),
+  filled: true,
+  fillColor: Color.fromARGB(49, 255, 135, 171),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15)),
+  ),
+);
+const TextStyle titleStyle =
+    TextStyle(fontSize: 15, fontWeight: FontWeight.w500);
+const TextStyle titleStyleOr =
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
---
+++ lib/view/login_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pet_adopt/services/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const SizedBox(
+            height: 50,
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  logoImage,
+                ),
+                const Text(
+                  "Adopt Me",
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 135, 171),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: Text(
+                    "Email or Phone number",
+                    style: titleStyle,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: textFieldDecoration,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: Text(
+                    "Password",
+                    style: titleStyle,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: textFieldDecoration,
+                  ),
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 16.0),
+                      child: Text(
+                        "Forgot Password?",
+                        style: titleStyle,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: mainColor,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 50, vertical: 20),
+                                  textStyle: const TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                              onPressed: () {},
+                              child: const Text(
+                                "Signin",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ))),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'OR',
+                    style: titleStyleOr,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(faceImage),
+                    Image.asset(googleImage),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Signup',
+                        style: TextStyle(
+                          color: mainColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
